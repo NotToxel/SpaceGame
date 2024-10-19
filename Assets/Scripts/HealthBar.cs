43a897a03909a0295f07bf1540101df91788fe6f
LouisChoong <120599663+LouisChoong@users.noreply.github.com>
2024-10-19 19:57:45 +0100

    UI Update
    + Health bar (Space to take dmg)
    + Oxygen bar (1 O2 depleted per second , max O2 set to 100)

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider healthSlider;
+    public Slider easeHealthSlider;
+    public float maxHP = 100f;
+    public float health;
+    private float lerpSpeed = 0.005f;
+
+    // Start is called before the first frame update
+    void Start() {
+        health = maxHP;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Update Health Slider
+        if(healthSlider.value != health) {
+            healthSlider.value = health;
+        }
+
+        // Update Ease Health Slider
+        if(healthSlider.value != easeHealthSlider.value) {
+            easeHealthSlider.value = Mathf.Lerp(easeHealthSlider.value, health, lerpSpeed);
+        }
+
+        // Testing
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            TakeDamage(10);
+        }
+    }
+
+    
+    void TakeDamage(float damage) {
+        health -= damage;
+        if(health < 0) {
+            health = 0;
+        }
+    }
+}
