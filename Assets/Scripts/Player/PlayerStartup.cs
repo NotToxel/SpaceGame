3480d6befd61567c4924e3947d9870499e847175
LouisChoong <120599663+LouisChoong@users.noreply.github.com>
2024-11-17 23:58:03 +0000

    Inventory
    + Hotbar UI
    + Hotbar script needs finishing

--- Assets/Scripts/Player/PlayerStartup.cs
+++ Assets/Scripts/Player/PlayerStartup.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class PlayerStartup : MonoBehaviour
 {
+    [SerializeField] private Hotbar hotbar;
+
     // === Instantiate Objects === //
     private Inventory inventory;
+
 
     void Awake()
     {
         inventory = new Inventory();
+        hotbar.SetInventory(inventory);
     }
 }
