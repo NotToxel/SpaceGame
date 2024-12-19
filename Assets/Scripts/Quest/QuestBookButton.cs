e91c79a374dc33711457541240a6d19994466ffb
SthefParis <74209864+SthefParis@users.noreply.github.com>
2024-12-19 20:28:47 +0000

    Quest Book
    - Players can now receive quests

---
+++ Assets/Scripts/Quest/QuestBookButton.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+using TMPro;
+
+public class QuestBookButton : MonoBehaviour
+{
+    [SerializeField] private GameObject questPage;
+    [SerializeField] private  TMP_Text questTextBox;
+    [SerializeField] private GameObject notification;
+    [SerializeField] private string[] noQuestsText;
+    private bool openBook;  
+
+    public void OpenQuestBook()
+    {
+        openBook = !openBook;
+        CreatePage();
+        WriteQuests();
+    }
+
+    private void CreatePage()
+    {
+        if (questPage != null && notification != null)
+        {
+            if (openBook)
+            {
+                questPage.SetActive(true);
+                notification.SetActive(false);
+            }
+            else{
+                questPage.SetActive(false);
+            }
+        }
+    }
+
+    private void WriteQuests()
+    {
+        if (questTextBox != null) 
+        {
+            if (MainManager.mainManager.questNames.Count == 0)
+            {
+                if (noQuestsText != null)
+                {
+                    int randomNum = (Random.Range(0, noQuestsText.Length));
+                    questTextBox.text = noQuestsText[randomNum];
+                }
+                else
+                {
+                    StringBuilder stringBuilder = new();
+                    foreach (string quest in MainManager.mainManager.questNames)
+                    {
+                        stringBuilder.AppendLine(quest);
+                    }
+                    questTextBox.text = stringBuilder.ToString();
+                }
+
+                questTextBox.rectTransform.sizeDelta = new Vector2(questTextBox.rectTransform.sizeDelta.x, questTextBox.preferredHeight);
+            }
+        }
+    }
+}
