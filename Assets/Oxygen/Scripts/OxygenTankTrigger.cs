dda56491c79c622bd37efaf7dffa983e5a45de7f
SthefParis <74209864+SthefParis@users.noreply.github.com>
2025-01-06 02:21:06 +0000

    Update Oxygen
    - Oxygen has been added to the map
    - Oxygen reappears after 20secs of consumption

--- Assets/Oxygen/Scripts/OxygenTankTrigger.cs
+++ Assets/Oxygen/Scripts/OxygenTankTrigger.cs
@@ -1,22 +1,27 @@
-/*****************
-This script checks if the player has collided with the oxygen tank.
-If the player has collided, the player will gain more oxygen 
-and the oxygen tank will be destroyed.
-*******************/
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class OxygenTankTrigger : MonoBehaviour
 {
-    public float oxygenTank = 10f;
+    [SerializeField] private float oxygenTank = 10f;
+    [SerializeField] private float wait = 20f;
+    public GameObject oxygen;
     public OxygenManager oxygenManager;
-    private void OnTriggerEnter(Collider other){
-        if(other.CompareTag("Player")){ //Checks if player has collided with tank
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) //Checks if player has collided with tank
+        {
             oxygenManager.AddOxygen(oxygenTank); //Increases player's oxygen
-            // Destroy(gameObject); //Destroys oxygen tank once player has used it
-            transform.position = new Vector3(3.4f, 1f, 3.85f);
+            oxygen.SetActive(false); // Deactivates the oxygen tank
+            StartCoroutine(ResetOxygen()); // Starts the coroutine to reset the tank
         }
+    }
+
+    private IEnumerator ResetOxygen()
+    {
+        yield return new WaitForSeconds(wait); // Waits for the specified time
+        oxygen.SetActive(true); // Reactivates the oxygen tank
     }
 }
