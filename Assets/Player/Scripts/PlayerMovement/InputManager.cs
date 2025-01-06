6a57e5ef17a597e2a337432f9f5a00a65140b46e
SthefParis <74209864+SthefParis@users.noreply.github.com>
2025-01-06 00:14:54 +0000

    Update tablet
    - Player has a number of side missions to complete

--- Assets/Player/Scripts/PlayerMovement/InputManager.cs
+++ Assets/Player/Scripts/PlayerMovement/InputManager.cs
@@ -33,8 +33,8 @@ public class InputManager : MonoBehaviour
             _instance = this;
         }
         playerControls = new PlayerControls();
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+        // Cursor.visible = false;
+        // Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void OnEnable()
